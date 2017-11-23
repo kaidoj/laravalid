5f7ea993ce860f6d4f41d3b70718eccfe24e58d8
Kaido J <jast112@gmail.com>
2017-11-23 16:15:51 +0200

    Fixed: typo bug

--- src/Bllim/Laravalid/FormBuilder.php
+++ src/Bllim/Laravalid/FormBuilder.php
@@ -126,7 +126,7 @@ class FormBuilder extends \Collective\Html\FormBuilder {
 	public function select($name, $list = [], $selected = null, array $selectAttributes = [], array $optionsAttributes = [])
 	{
 		$optionsAttributes = $this->converter->convert(Helper::getFormAttribute($name)) + $optionsAttributes;
-+        	$selectAttributes  = $this->converter->convert(Helper::getFormAttribute($name)) + $selectAttributes;
+        	$selectAttributes  = $this->converter->convert(Helper::getFormAttribute($name)) + $selectAttributes;
 		
 		return parent::select($name, $list, $selected, $selectAttributes, $optionsAttributes);
 	}
